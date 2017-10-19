3aec067965dbdaf2b51a5bf93c59d46b6b868ba7
liumapp <liumapp.com@gmail.com>
2017-10-19 16:16:16 +0800

    add target ignore

--- backend/web/src/main/java/com/liumapp/helloSv/backend/web/Main.java
+++ backend/web/src/main/java/com/liumapp/helloSv/backend/web/Main.java
@@ -1,17 +1,24 @@
 package com.liumapp.helloSv.backend.web;
 
+import com.liumapp.helloSv.backend.web.utils.MyMapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by liumapp on 10/9/17.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
-@Import(com.liumapp.helloSv.common.Main.class)
-public class Main {
+@Controller
+@EnableWebMvc
+@MapperScan(basePackages = "com.liumapp.helloSv.backend.web.mapper" , markerInterface = MyMapper.class)
+@SpringBootApplication
+public class Main extends WebMvcConfigurerAdapter{
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class , args);
